3d4a4884a360973acca2efbcec4e9efd60cfef16
Zach Saunders <zachisaunders12@gmail.com>
2019-08-27 12:18:09 -0400

    Added to source file

--- src/calculator.java
+++ src/calculator.java
@@ -1,2 +1,10 @@
 public class calculator {
+    public static void main(String[] args) {
+        float homeWeight;
+        float classWeight;
+        float testWeight;
+        System.out.println("Welcome to the Grade Calculator. This program has a lot of fun features.");
+        System.out.println("Let's start with what you want to do");
+        System.out.println();
+    }
 }
