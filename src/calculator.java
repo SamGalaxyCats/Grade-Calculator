90e0293524c49e9de0f33a1c883899bdf1ad1490
SamGalaxyCats <30423007+SamGalaxyCats@users.noreply.github.com>
2019-08-27 12:25:18 -0400

    Update calculator.java

--- src/calculator.java
+++ src/calculator.java
@@ -1,8 +1,12 @@
-public class calculator {
-    public static void main(String[] args) {
-        float homeWeight;
-        float classWeight;
-        float testWeight;
+public class calculator 
+{
+    public static void main(String[] args) 
+    {
+        float homeWeight; //constant
+        float classWeight; //constant
+        float testWeight; //constant
+        float homeScore; //numerator
+        float homePossible; //denominator
         System.out.println("Welcome to the Grade Calculator. This program has a lot of fun features.");
         System.out.println("Let's start with what you want to do");
         System.out.println();
