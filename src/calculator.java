0193036e95f52045c9a990b811aa4546cf404af2
Zach Saunders <zachisaunders12@gmail.com>
2019-08-28 12:08:40 -0400

    Imported scanner, fleshed out user selection.

--- src/calculator.java
+++ src/calculator.java
@@ -1,4 +1,6 @@
-public class calculator 
+import java.util.Scanner;
+
+public class calculator
 {
     public static void main(String[] args) 
     {
@@ -7,8 +9,18 @@ public class calculator
         float testWeight; //constant
         float homeScore; //numerator
         float homePossible; //denominator
-        System.out.println("Welcome to the Grade Calculator. This program has a lot of fun features.");
-        System.out.println("Let's start with what you want to do");
+        Scanner choiceChecker = new Scanner(System.in); // Scanner to be used in any user interaction
+
+        // Introductory period, where user makes choice.
+        System.out.println("\nWelcome to the Grade Calculator. This program has a lot of fun features.\n");
+        System.out.println("1. GPA Calculator");
+        System.out.println("2. Current grade with assignment weighting");
+        System.out.println("3. Score required on exam to get given letter grade");
+        System.out.print("\n");
+        System.out.println("Let's start with what you want to do:");
         System.out.println();
+        int userChoice = choiceChecker.nextInt();
+
+        // At this point we need each of the functions
     }
 }
